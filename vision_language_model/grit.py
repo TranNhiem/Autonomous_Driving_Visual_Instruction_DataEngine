da16f424960d9d891c74e69e25ef4f0207c74c75
TranNhiem <tvnhiemhmus@g.ncu.edu.tw>
2023-07-12 10:46:22 +0800

    Update_Object_Dense_Caption

---
+++ vision_language_model/grit.py
@@ -0,0 +1,7 @@
+'''
+TranNhiem 2023/07/12 
+This Code features 
++ Abstract Object Detection & Localization via Dense captioning information 
+GRIT --> Image Dense Caption
+
+'''
--- vision_language_model/kosmos.py
+++ vision_language_model/kosmos.py
@@ -4,3 +4,5 @@ TranNhiem 2023/07/10
 The implementaiton of kosmo for extract the Visual Grounding Information 
 
 '''
+import os
+from models.grit_src.image_dense_captions import image_caption_api
